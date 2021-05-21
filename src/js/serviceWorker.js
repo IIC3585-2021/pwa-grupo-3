c92bdd081f8549c070ba389893d4e1bf20a6c94d
Macarena Concha <maconcha4@uc.cl>
2021-05-21 19:35:39 -0400

    fix

--- src/js/serviceWorker.js
+++ src/js/serviceWorker.js
@@ -1,4 +1,4 @@
-const staticDevCoffee = "dev-coffee-site-v1"
+const staticTwitterDev = "dev-coffee-site-v1"
 const assets = [
   "/",
   "/index.html",
@@ -8,7 +8,7 @@ const assets = [
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticTwitterDev).then(cache => {
       cache.addAll(assets)
     })
   )
